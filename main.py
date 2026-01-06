2f5171babddfe987c9bfc567c5b2d41d8cd345da
SvanikDamle11 <damlesvanik11@gmail.com>
2026-01-06 17:53:46 +0530

    py1

---
+++ main.py
@@ -0,0 +1,16 @@
+a = 10 # integer data type
+b = 20.56 # float data type
+name ="Svanik" # string data type
+checked = True
+print("The name of the student is: ",name)
+x = 10
+y = 20
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x//y)
+name1 = "Svanik"
+name2 = " Damle"
+print(name1 + name2)
