072ca71c33855b518361c1fbbfb8500af83b4513
SvanikDamle11 <damlesvanik11@gmail.com>
2026-01-29 17:57:50 +0530

    py6

--- main10.py
+++ main10.py
@@ -3,24 +3,10 @@ class face:
     def __init__(self, x, y):
         self.size = (90)
         self.coord = (x , y)
+        self.nosesize = "small"
 
     def setSize(self, radius):
-        self.size = radius 
-
-    def reset(self):
-        penup()
-        goto(self.coord)
-
-    def draw(self, reset):
-        self.reset()
-        pensize(3)
-        speed(0)
-
-    def draw(self):
-        self.reset()
-        pensize(3)
-        speed(0)
-        self.drawOutline()
+        self.size = radius
 
     def draw(self):
         self.reset()
@@ -29,10 +15,52 @@ class face:
         self.drawOutline()
         self.drawEye(135)
         self.drawEye (45)
-        self.drawlose()
+        self.drawnose()
         self.drawMouth()
         pensize(5)
+
+    def reset(self):
+        penup()
+        goto(self.coord)
+        setheading(0)
+
+    def drawEye(self, turn):
+        penup()
+        left(turn)
+        forward(self.size / 2)
+        pendown()
+        dot(self.size / 10)
+        self.reset()
+
+    def drawMouth(self):
+        penup()
+        right(135)
+        forward(self.size/1.7)
+        left(90)
+        pendown()
+        circle(self.size/1.7,90)
+        self.reset()
+
+    def drawnose(self):
+        if self.nosesize == 'large' :
+            dot(self.size/2, "grey")
+        elif self.nosesize == 'small' :
+            dot(self.size/6, "grey")
+        else:
+            dot(self.size/4, "grey")
+            self.reset()
+
+    def drawOutline(self):
+        penup()
+        forward(self.size)
+        left(90)
+        pendown()
+        circle(self.size)
+        self.reset()
     
 
 
 f1 = face(0 , 0)
+f1.draw()
+showturtle()
+done()
---
+++ main11.py
@@ -0,0 +1,7 @@
+my_dict = {}
+my_dict = {1:'apple', 2:'ball'}
+my_dict = {'name':'Svanik', 'age':'11'}
+my_dict={'name': 'John', 1: [2, 4, 3]}
+new_dict={'name': 'John', 'age': '34', 1: [2, 4, 3]}
+print(new_dict['name'])
+print(new_dict.get('name'))
