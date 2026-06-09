044bbe848d01e95be8d3ffcb2b43bea5f176e21f
SvanikDamle11 <damlesvanik11@gmail.com>
2026-06-09 17:43:17 +0530

    RPS

--- rps..py
+++ rps..py
@@ -1,26 +1,26 @@
 import random
 print("Rock Paper Scissors Game")
 options = ['Rock', 'Paper', 'Scissors']
-computers_choice = random.choice(options)
-users_choice = input("Choose Rock Paper Scissors :")
+while True:
+    computers_choice = random.choice(options)
+    users_choice = input("Choose Rock Paper Scissors :")
+    print("Computers choice:", computers_choice)
+    print("Users choice: ",users_choice)
 
-print("Computers choice:", computers_choice)
-print("Users choice: ",users_choice)
-
-if users_choice == computers_choice:
-    print("Tie!")
-elif users_choice == "Rock":
-    if computers_choice == "Scissors":
-        print("You win!")
-    else: # paper
-        print("You lose!")
-elif users_choice == "Paper":
-    if computers_choice == "Rock":
-        print("You win!")
-    else: # scissors
-        print("You lose!")
-elif users_choice == "Scissors":
-    if computers_choice == "Paper":
-        print("You win!")
-    else: # rock
-        print("You lose!")
+    if users_choice == computers_choice:
+        print("Tie!")
+    elif users_choice == "Rock":
+        if computers_choice == "Scissors":
+            print("You win!")
+        else: # paper
+            print("You lose!")
+    elif users_choice == "Paper":
+        if computers_choice == "Rock":
+            print("You win!")
+        else: # scissors
+            print("You lose!")
+    elif users_choice == "Scissors":
+        if computers_choice == "Paper":
+            print("You win!")
+        else: # rock
+            print("You lose!")
